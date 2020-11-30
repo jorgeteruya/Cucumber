e3bfd6432f56c0d3a38687e56ae30c35eddc1026
jorgeteruya <40158063+jorgeteruya@users.noreply.github.com>
2020-11-30 01:16:01 -0300

    Aula de regex

--- Curso_Cucumber_R/README_AULA.txt
+++ Curso_Cucumber_R/README_AULA.txt
@@ -1,3 +1,13 @@
-Aula Asserts
+Aula Regex
 
-Validação dos testes de forma automatizada
+No site abaixo é possivel testar a lógica da linguagem natural construida no cucumber
+https://jex.im/regulex/#!flags=&re=%5E(a%7Cb)*%3F%24
+exemplo:
+^que o valor do contador eh (\d+)$
+
+Aqui validamos o mapeamento
+https://regex101.com/
+exemplo: (\d+\/\d+\/\d+) para pegar data
+
+sendo que d+ são um ou mais digitos 
+e d{2} são exatamente dois dígitos 
--- Curso_Cucumber_R/src/test/java/AprenderCucumber.java
+++ Curso_Cucumber_R/src/test/java/AprenderCucumber.java
@@ -1,12 +1,20 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
 import org.junit.Assert;
 
 import io.cucumber.java.pt.*;
+import io.cucumber.core.cli.Main;
 
 public class AprenderCucumber {
 	
 	@Dado("que eu crie o arquivo corretamente")
 	public void que_eu_crie_o_arquivo_corretamente() {
-	    System.out.println("\ndeu certo\n");
+	    //System.out.println("\ndeu certo\n");
 	}
 
 	@Quando("executá-lo")
@@ -20,7 +28,7 @@ public class AprenderCucumber {
 	}
 	
 	
-//------------------Aula 10----------------------
+//------------------Aula 10-11----------------------
 	
 	private int contador = 0;
 	
@@ -36,15 +44,61 @@ public class AprenderCucumber {
 
 	@Então("o valor do contador será {int}")
 	public void oValorDoContadorSerá(int arg1) {
-		System.out.println("int1: " + arg1);
+		System.out.println("arg1s: " + arg1);
 		System.out.println("Contador: " + arg1);
-		System.out.println(arg1 == contador);
+		
 		//valida se é true
 		//Assert.assertTrue(arg1 == contador);
 		
 		//sendo que arg1 é o valor esperado e contador é o valor obtido
-		System.out.println("\nAssert.assertEquals");
-		Assert.assertEquals(arg1, contador);
+		
+		
+		if(arg1 == contador) {
+			System.out.println("Passou no teste");
+		}else {
+			System.out.println("Reprovado");
+			System.out.println("\nAssert.assertEquals");
+			Assert.assertEquals(arg1, contador);
+		}
+	}
+
+//------------------Aula 12----------------------
+	
+	Date entrega = new Date();
+	Calendar cal = Calendar.getInstance();
+	//@Dado("que o prazo é dia {int}\\/{int}\\/{int}")
+	@Dado("que o prazo é dia (d{2}/d{2}/d{4})$")
+	public void queOPrazoÉDia(int dia, int mes, int ano) {
+	    
+	    cal.set(Calendar.DAY_OF_MONTH, dia);
+	    //no java janeiro é 0
+	    cal.set(Calendar.MONTH, mes - 1);
+	    cal.set(Calendar.YEAR, ano);
+	    
+	    entrega = cal.getTime();
+	}
+
+	//@Quando("a entrega atrasa em {int} dias")
+	@Quando("a entrega atrasa em (\\d+) (dia|dias|mes|meses)$")
+	public void aEntregaAtrasaEmDias(int dias, String tempo) {
+		cal.setTime(entrega);	    
+		if(tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, dias);
+		}else if (tempo.equals("meses")) {
+			cal.add(Calendar.MONTH, dias);
+		}
+		
+	    entrega = cal.getTime();
+	}
+
+	//exemplo da aula (\\d{2}\\/\\d{2}\\/\\d{4})
+	//@Então("a entrega será efetuada e {int}\\/{int}\\/{int}")
+	@Então("a entrega será efetuada e (\\d{2}/d{2}/d{4})$")
+	public void aEntregaSeráEfetuadaE(String data) {
+	    DateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+	    String dataFormatada = formatoData.format(entrega);
+	    Assert.assertEquals(data, formatoData);
+	    
 	}
 
 
